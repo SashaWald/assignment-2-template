6fc727bfc74a7c39f348177c42c292fe64a42784
Sasha Wald <sashawald@gmail.com>
2020-04-26 17:59:19 -0400

    edited fibonacciNums.js

--- fibonacciNums.js
+++ fibonacciNums.js
@@ -1,5 +1,21 @@
+  
 const fibonacciNums = (n) => {
-
+    let fibonacci = [0,1]
+    if (n === 0){
+        return []
+    }
+    else if (n === 1){
+        return [0]
+    }
+    else if (n === 2){
+        return fibonacci
+    }
+    else{
+        for (i = 2; i < n; i++){
+            fibonacci.push(fibonacci[i-1] + fibonacci[i-2])
+        }
+        return fibonacci
+    }    
 };
 
 module.exports = fibonacciNums;
