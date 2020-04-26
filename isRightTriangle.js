875575468af4dc6f1b5a612b0212ac791369988f
Sasha Wald <sashawald@gmail.com>
2020-04-26 17:51:32 -0400

    edited isRightTriangle.js

--- isRightTriangle.js
+++ isRightTriangle.js
@@ -1,5 +1,16 @@
 const isRightTriangle = (a, b, c) => {
-
+    if(Math.pow(a,2)+Math.pow(b,2) === Math.pow(c,2)){
+        return true
+    }
+    else if(Math.pow(b,2)+Math.pow(c,2) === Math.pow(a,2)){
+        return true
+    }
+    else if(Math.pow(a,2)+Math.pow(c,2) === Math.pow(b,2)){
+        return true
+    }
+    else{
+        return false
+    }
 };
 
 module.exports = isRightTriangle;
