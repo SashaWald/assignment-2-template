452e4905cd01147d2e26d450ed8bc15ea5a412fd
Sasha Wald <sashawald@gmail.com>
2020-04-26 18:04:18 -0400

    edited getFrequencies.js

--- getFrequencies.js
+++ getFrequencies.js
@@ -1,5 +1,13 @@
 const getFrequencies = (arr) => {
-
+    let frequencies = {}
+    for (i = 0; i < arr.length; i++){
+        if(arr[i] in frequencies){
+            frequencies[arr[i]]++;
+        }
+        else{
+            frequencies[arr[i]]=1;
+        }
+    }
+    return frequencies
 };
-
 module.exports = getFrequencies;
